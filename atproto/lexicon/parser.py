b01a50e774b2be738dbcfd1c3e55e56aa4ab7ac6
Ilya (Marshal) <ilya@marshal.dev>
2023-04-28 01:38:02 +0200

    fix raising of parsing errors

--- atproto/lexicon/parser.py
+++ atproto/lexicon/parser.py
@@ -7,6 +7,8 @@ import dacite
 import models
 from exceptions import UnknownDefinitionTypeError, UnknownPrimitiveTypeError
 
+_DEBUG = os.environ.get('DEBUG', '0') == '1'
+
 _PATH_TO_LEXICONS = Path(__file__).parent.parent.parent.joinpath('lexicons').absolute()
 
 _LEX_DEFINITION_TYPE_TO_CLASS = {
@@ -27,24 +29,24 @@ _LEX_PRIMITIVE_TYPE_TO_CLASS = {
 
 def _lex_definition_type_hook(data: dict) -> models.LexDefinition:
     try:
-        definition_class = _LEX_DEFINITION_TYPE_TO_CLASS.get(models.LexDefinitionType(data['type']))
+        definition_class = _LEX_DEFINITION_TYPE_TO_CLASS[models.LexDefinitionType(data['type'])]
     except (ValueError, KeyError):
-        raise UnknownDefinitionTypeError(data['type'])
+        raise UnknownDefinitionTypeError('Unknown definition type ' + data['type'])
 
     return lexicon_parse(data, definition_class)
 
 
 def _lex_primitive_type_hook(data: dict) -> models.LexPrimitive:
     try:
-        primitive_class = _LEX_PRIMITIVE_TYPE_TO_CLASS.get(models.LexPrimitiveType(data['type']))
+        primitive_class = _LEX_PRIMITIVE_TYPE_TO_CLASS[models.LexPrimitiveType(data['type'])]
     except (ValueError, KeyError):
-        raise UnknownPrimitiveTypeError
+        raise UnknownPrimitiveTypeError('Unknown primitive type ' + data['type'])
 
     return lexicon_parse(data, primitive_class)
 
 
 _TYPE_HOOKS = {models.LexDefinition: _lex_definition_type_hook, models.LexPrimitive: _lex_primitive_type_hook}
-_DEFAULT_DACITE_CONFIG = dacite.Config(cast=[Enum], type_hooks=_TYPE_HOOKS)
+_DEFAULT_DACITE_CONFIG = dacite.Config(cast=[Enum], type_hooks=_TYPE_HOOKS, check_types=_DEBUG)
 
 
 def lexicon_parse(data: dict, data_class=None):
@@ -62,7 +64,23 @@ def main(lexicon_path: Path):
 
 
 if __name__ == '__main__':
+    lexicons_to_test = None
+
+    parsing_errors_count = 0
     for _, _, lexicons in os.walk(_PATH_TO_LEXICONS):
         for lexicon in lexicons:
-            lexicon_doc = main(_PATH_TO_LEXICONS.joinpath(lexicon))
-            print(lexicon_doc)
+            if not lexicon.endswith('.json'):
+                continue
+
+            if lexicons_to_test and lexicon not in lexicons_to_test:
+                continue
+
+            try:
+                lexicon_doc = main(_PATH_TO_LEXICONS.joinpath(lexicon))
+                # print(lexicon_doc)
+            except Exception as e:
+                parsing_errors_count += 1
+                print(lexicon, str(e))
+
+    print('Errors count:', parsing_errors_count)
+    assert parsing_errors_count == 0
