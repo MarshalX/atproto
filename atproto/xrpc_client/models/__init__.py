552d68152ea6fdc18bff3475d3d6899037d2b4aa
Ilya (Marshal) <ilya@marshal.dev>
2023-05-03 18:33:57 +0200

    add impl of BlobRef

--- atproto/xrpc_client/models/__init__.py
+++ atproto/xrpc_client/models/__init__.py
@@ -1,6 +1,8 @@
+import dataclasses
+import json
 from typing import Type, TypeVar
 
-from dacite import exceptions, from_dict
+from dacite import Config, exceptions, from_dict
 from exceptions import (
     MissingValueError,
     ModelError,
@@ -17,6 +19,8 @@ from xrpc_client.request import Response
 
 M = TypeVar('M')
 
+_DACITE_CONFIG = Config(type_hooks={BlobRef: lambda ref: BlobRef.from_dict(ref)})
+
 
 def get_or_create_model(model_data: Union[dict], model: Type[M]) -> Optional[M]:
     if model_data is None:
@@ -29,7 +33,7 @@ def get_or_create_model(model_data: Union[dict], model: Type[M]) -> Optional[M]:
         # validate unexpected fields
         model(**model_data)
 
-        return from_dict(model, model_data)
+        return from_dict(model, model_data, config=_DACITE_CONFIG)
     except TypeError as e:
         msg = str(e).replace('__init__()', model.__name__)
         raise UnexpectedFieldError(msg)
@@ -48,3 +52,48 @@ def get_response_model(response: Response, model: Type[M]) -> Optional[M]:
         return response.status_code
 
     return get_or_create_model(response.content, model)
+
+
+def _handle_dict_key(key: str) -> str:
+    if key == '_type':  # special SDK's name to resolve issues with $ symbol in Python
+        return '$type'
+
+    return key
+
+
+def _handle_dict_value(ref: Any) -> dict:
+    # TODO(MarshalX): add CID?
+    if isinstance(ref, BlobRef):
+        return ref.to_dict()
+
+    return ref
+
+
+def _model_as_dict_factory(value):
+    # exclude None values and process keys and values
+    return {_handle_dict_key(k): _handle_dict_value(v) for k, v in value if v is not None}
+
+
+def get_model_as_dict(model) -> dict:
+    if model == BlobRef:
+        return model.to_dict()
+
+    if not dataclasses.is_dataclass(model):
+        raise ModelError('Invalid model')
+
+    return dataclasses.asdict(model, dict_factory=_model_as_dict_factory)
+
+
+def get_model_as_json(model) -> str:
+    return json.dumps(get_model_as_dict(model))
+
+
+def is_json(json_data: Union[str, bytes]):
+    if isinstance(json_data, bytes):
+        json_data.decode('UTF-8')
+
+    try:
+        json.loads(json_data)
+        return True
+    except Exception:  # noqa
+        return False
--- atproto/xrpc_client/models/blob_ref.py
+++ atproto/xrpc_client/models/blob_ref.py
@@ -1,3 +1,25 @@
+from multiformats import CID
+
+
 class BlobRef:
-    # TODO(MarshalX): implement
-    ...
+    def __init__(self, blob_type: str, mime_type: str, ref: str, size: int):
+        self.blob_type = blob_type
+        self.mime_type = mime_type
+        self.ref = ref
+        self.size = size
+
+    @property
+    def cid(self) -> CID:
+        return CID.decode(self.ref)
+
+    @classmethod
+    def from_dict(cls, data: dict) -> 'BlobRef':
+        return cls(
+            blob_type=data['$type'],
+            mime_type=data['mimeType'],
+            ref=data['ref']['$link'],
+            size=int(data['size']),
+        )
+
+    def to_dict(self) -> dict:
+        return {'$type': self.blob_type, 'mimeType': self.mime_type, 'size': self.size, 'ref': {'$link': self.ref}}
