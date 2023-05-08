ffc5aca7957bfc27320ba598160ac335a9246299
Ilya (Marshal) <ilya@marshal.dev>
2023-05-08 20:26:05 +0200

    add home_timeline and profile_posts examples

---
+++ examples/advanced_usage/home_timeline.py
@@ -0,0 +1,28 @@
+from atproto import Client
+
+
+def main():
+    client = Client()
+    client.login('my-handle', 'my-password')
+
+    print('Home (Following):\n\n')
+
+    # Get "Home" page. Use pagination (cursor + limit) to fetch all posts
+    timeline = client.bsky.feed.get_timeline({'algorithm': 'reverse-chronological'})
+    for feed_view in timeline.feed:
+        author = feed_view.post.author
+
+        action = 'New Post'
+        action_by = author.displayName
+        if feed_view.reason:
+            action = type(feed_view.reason).__name__.replace('Reason', '')
+            action_by = feed_view.reason.by.displayName
+
+        post = feed_view.post.record
+        author = feed_view.post.author
+
+        print(f'[{action} by {action_by}] Post author: {author.displayName}. Posted at {post.createdAt}. Post text: {post.text}')
+
+
+if __name__ == '__main__':
+    main()
