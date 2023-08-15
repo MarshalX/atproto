2814f013f038ac7183132389033cffc5981df1f9
Ilya (Marshal) <ilya@marshal.dev>
2023-08-15 12:59:14 +0200

    fix example

--- examples/advanced_usage/update_profile.py
+++ examples/advanced_usage/update_profile.py
@@ -1,12 +1,13 @@
 import os
 
 from atproto import Client, models
+from atproto.exceptions import BadRequestError
 
 
 def main():
     client = Client()
     client.login(os.environ['USERNAME'], os.environ['PASSWORD'])
-    
+
     try:
         current_profile_record = client.com.atproto.repo.get_record(
             models.ComAtprotoRepoGetRecord.Params(
@@ -16,28 +17,31 @@ def main():
             )
         )
         current_profile = current_profile_record.value
-        swapRecord = current_profile_record.cid
-    except:
-        current_profile = models.AppBskyActorProfile.Main
-        swapRecord = None
+        swap_record_cid = current_profile_record.cid
+    except BadRequestError:
+        current_profile = swap_record_cid = None
+
+    old_description = None
+    old_display_name = None
+    if current_profile:
+        old_description = current_profile.description
+        old_display_name = current_profile.displayName
 
     # set new values to update
     new_description = None
     new_display_name = None
-    new_avatar = None
-    new_banner = None
 
     client.com.atproto.repo.put_record(
         models.ComAtprotoRepoPutRecord.Data(
             collection=models.ids.AppBskyActorProfile,
             repo=client.me.did,
             rkey='self',
-            swapRecord=swapRecord,
+            swapRecord=swap_record_cid,
             record=models.AppBskyActorProfile.Main(
-                avatar=new_avatar or current_profile.avatar,
-                banner=new_banner or current_profile.banner,
-                description=new_description or current_profile.description,
-                displayName=new_display_name or current_profile.displayName,
+                avatar=current_profile.avatar,  # keep old avatar. to set a new one, you should upload blob first
+                banner=current_profile.banner,  # keep old banner. to set a new one, you should upload blob first
+                description=new_description or old_description,
+                displayName=new_display_name or old_display_name,
             ),
         )
     )
