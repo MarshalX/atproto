31163f16a37c1879aa3b5653ee0e6027084cc705
Ilya (Marshal) <ilya@marshal.dev>
2023-09-06 23:40:22 +0200

    remove typing_validation hack

--- typing_validation/__init__.py
+++
@@ -1,5 +0,0 @@
-# FIXME(MarshalX): remove this dirty hack when they fix it or when I'll migrate to better lib
-
-from typing_validation.validation import validate
-
-__all__ = ['validate']
--- typing_validation/validation.py
+++
@@ -1,5 +0,0 @@
-from typing import Any
-
-
-def validate(_: Any, __: Any) -> None:
-    ...
