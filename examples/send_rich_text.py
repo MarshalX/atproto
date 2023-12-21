8b881f67cdec345dea93a9146953bdda249739b2
Ilya Siamionau <ilya@marshal.dev>
2023-12-21 19:33:59 +0100

    Add package structure to README (#216)

--- examples/send_rich_text.py
+++ examples/send_rich_text.py
@@ -1,5 +1,4 @@
-from atproto import Client
-from atproto_client.utils import TextBuilder
+from atproto import Client, client_utils
 
 # To send links as "link card" or "quote post" look at the advanced_usage/send_embed.py example.
 # There is a more advanced way to send rich text without helper class in the advanced_usage/send_rich_text.py example.
@@ -9,7 +8,7 @@ def main() -> None:
     client = Client()
     client.login('my-handle', 'my-password')
 
-    text_builder = TextBuilder()
+    text_builder = client_utils.TextBuilder()
     text_builder.tag('This is a rich message. ', 'atproto')
     text_builder.text('I can mention ')
     text_builder.mention('account', 'did:plc:kvwvcn5iqfooopmyzvb4qzba')
@@ -20,7 +19,7 @@ def main() -> None:
     client.send_post(text_builder)  # same with send_image method
 
     # Same with chaining:
-    client.send_post(TextBuilder().text('Test msg using ').link('Python SDK', 'https://atproto.blue/'))
+    client.send_post(client_utils.TextBuilder().text('Test msg using ').link('Python SDK', 'https://atproto.blue/'))
 
 
 if __name__ == '__main__':
