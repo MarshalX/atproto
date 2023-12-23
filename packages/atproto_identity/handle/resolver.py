e771389640dcb122824e5d990c0abeb79f1cad9e
Ilya (Marshal) <ilya@marshal.dev>
2023-12-23 02:20:47 +0100

    implement handle resolver

--- packages/atproto_identity/handle/resolver.py
+++ packages/atproto_identity/handle/resolver.py
@@ -1,5 +1,19 @@
 import typing as t
 
+import dns.asyncresolver
+import dns.resolver
+import httpx
+from dns.exception import DNSException
+
+from atproto_identity.exceptions import DidNotFoundError
+
+_ATPROTO_SUBDOMAIN = '_atproto'
+_ATPROTO_RECORD_TYPE = 'TXT'
+_ATPROTO_TXT_PREFIX = 'did='
+_ATPROTO_TXT_PREFIX_LEN = len(_ATPROTO_TXT_PREFIX)
+_ATPROTO_WELL_KNOWN_PATH = '/.well-known/atproto-did'
+_ATPROTO_DID_PREFIX = 'did:'
+
 
 class HandleResolver:
     def __init__(
@@ -8,10 +22,110 @@ class HandleResolver:
         backup_nameservers: t.Optional[t.List[str]] = None,
     ) -> None:
         self._timeout = timeout
-        self._backup_nameservers = backup_nameservers
+        self._backup_nameservers = backup_nameservers  # TODO(MarshalX): implement
 
-    def resolve(self, handle: str) -> t.Optional[dict]:
-        raise NotImplementedError
+        self._dns_resolver = dns.resolver
+        self._dns_resolver_async = dns.asyncresolver
 
-    async def resolve_async(self, handle: str) -> t.Optional[dict]:
-        raise NotImplementedError
+        self._http_client = httpx.Client()
+        self._http_client_async = httpx.AsyncClient()
+
+    def resolve(self, handle: str) -> t.Optional[str]:
+        dns_resolve = self.resolve_dns(handle)
+        if dns_resolve:
+            return dns_resolve
+
+        http_resolve = self.resolve_http(handle)
+        if http_resolve:
+            return http_resolve
+
+        # TODO(MarshalX): add resolve DNS backup nameservers
+        return None
+
+    def ensure_resolve(self, handle: str) -> str:
+        did = self.resolve(handle)
+        if not did:
+            raise DidNotFoundError(f'Unable to resolve handle: {handle}')
+
+        return did
+
+    @staticmethod
+    def _find_text_record(answers: dns.resolver.Answer) -> t.Optional[str]:
+        for answer in answers:
+            for item in answer.strings:
+                value = item.decode('UTF-8')
+                if value.startswith(_ATPROTO_TXT_PREFIX):
+                    return value[_ATPROTO_TXT_PREFIX_LEN:]
+
+        return None
+
+    @staticmethod
+    def _get_qname(handle: str) -> str:
+        return f'{_ATPROTO_SUBDOMAIN}.{handle}'
+
+    @staticmethod
+    def _get_did_from_text_response(text: str) -> t.Optional[str]:
+        lines = text.splitlines()
+        if not lines:
+            return None
+
+        first_line = lines[0].strip()
+        if first_line.startswith(_ATPROTO_DID_PREFIX):
+            return first_line
+
+        return None
+
+    def resolve_dns(self, handle: str) -> t.Optional[str]:
+        try:
+            answers = self._dns_resolver.resolve(self._get_qname(handle), _ATPROTO_RECORD_TYPE, lifetime=self._timeout)
+        except DNSException:
+            return None
+
+        return self._find_text_record(answers)
+
+    def resolve_http(self, handle: str) -> t.Optional[str]:
+        try:
+            response = self._http_client.get(f'https://{handle}{_ATPROTO_WELL_KNOWN_PATH}', timeout=self._timeout)
+            response.raise_for_status()
+            return self._get_did_from_text_response(response.text)
+        except httpx.HTTPError:
+            return None
+
+    async def resolve_async(self, handle: str) -> t.Optional[str]:
+        dns_resolve = await self.resolve_dns_async(handle)
+        if dns_resolve:
+            return dns_resolve
+
+        http_resolve = await self.resolve_http_async(handle)
+        if http_resolve:
+            return http_resolve
+
+        # TODO(MarshalX): add resolve DNS backup nameservers
+        return None
+
+    async def ensure_resolve_async(self, handle: str) -> str:
+        did = await self.resolve_async(handle)
+        if not did:
+            raise DidNotFoundError(f'Unable to resolve handle: {handle}')
+
+        return did
+
+    async def resolve_dns_async(self, handle: str) -> t.Optional[str]:
+        try:
+            answers = await self._dns_resolver_async.resolve(
+                self._get_qname(handle), _ATPROTO_RECORD_TYPE, lifetime=self._timeout
+            )
+        except DNSException:
+            return None
+
+        return self._find_text_record(answers)
+
+    async def resolve_http_async(self, handle: str) -> t.Optional[str]:
+        try:
+            response = await self._http_client_async.get(
+                f'https://{handle}{_ATPROTO_WELL_KNOWN_PATH}', timeout=self._timeout
+            )
+            response.raise_for_status()
+            return self._get_did_from_text_response(response.text)
+        except httpx.HTTPError:
+            return None
