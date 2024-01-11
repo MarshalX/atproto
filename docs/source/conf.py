63beb327691962943437656d5aa590e8f4ad584b
Ilya (Marshal) <ilya@marshal.dev>
2024-01-11 20:59:03 +0100

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -126,9 +126,10 @@ ogp_enable_meta_description = True
 # Pydantic models
 autodoc_pydantic_model_undoc_members = True
 autodoc_pydantic_model_show_json = True
-autodoc_pydantic_settings_show_json = False
 autodoc_pydantic_model_show_config_summary = False
-autodoc_pydantic_settings_show_validator_summary = False
+autodoc_pydantic_model_show_validator_summary = False
+autodoc_pydantic_field_list_validators = False
+autodoc_pydantic_settings_show_json = False
 
 
 autosectionlabel_prefix_document = True
