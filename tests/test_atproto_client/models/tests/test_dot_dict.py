39bc3ec63ef227260d9969bce8fa6a175066bbbe
Ilya Siamionau <ilya@marshal.dev>
2024-01-16 22:58:54 +0100

    Fix input data mutation in DotDict (#246)

--- tests/test_atproto_client/models/tests/test_dot_dict.py
+++ tests/test_atproto_client/models/tests/test_dot_dict.py
@@ -1,6 +1,37 @@
 from atproto_client.models.dot_dict import DotDict
 
 
+def test_dot_dict_input_mutation() -> None:
+    test_data = {'a': 1, 'b': {'c': 2}, 'd': [{'e': 3}, 4, 5]}
+    model = DotDict(test_data)
+
+    model['a'] = 2
+    assert model['a'] == 2
+    assert test_data['a'] == 1
+
+    model.b.c = 3
+    assert model.b.c == 3
+    assert test_data['b']['c'] == 2
+
+    model['d'][0]['e'] = 6
+    assert model['d'][0]['e'] == 6
+    assert test_data['d'][0]['e'] == 3
+
+
+def test_dot_dict_output_mutation() -> None:
+    model = DotDict({'a': 1, 'b': {'c': 2}, 'd': [{'e': 3}, 4, 5]})
+    output_dict = model.to_dict()
+
+    output_dict['a'] = 2
+    assert model['a'] == 1
+
+    output_dict['b']['c'] = 3
+    assert model.b.c == 2
+
+    output_dict['d'][0]['e'] = 6
+    assert model['d'][0]['e'] == 3
+
+
 def test_dot_dict() -> None:
     expected_created_at = '2021-03-30T12:00:00Z'
     expected_snake_case = 'test'
@@ -12,6 +43,14 @@ def test_dot_dict() -> None:
         'createdAt': expected_created_at,
         'snake_case': expected_snake_case,
     }
+    expected_edited_test_data = {
+        'a': 1,
+        'b': {'c': 2},
+        'createdAt': 'blabla',
+        'd': [{'e': 6}, 4, 5],
+        'snake_case': 'test',
+    }
+
     model = DotDict(test_data)
 
     assert isinstance(model, DotDict)
@@ -57,4 +96,12 @@ def test_dot_dict() -> None:
     assert model['snakeCase'] == expected_snake_case
 
     assert DotDict(test_data) == DotDict(test_data)
-    assert model.to_dict() == test_data
+
+    assert isinstance(model.to_dict(), dict)
+    assert model.to_dict() == expected_edited_test_data
+    assert model.to_dict() == model.to_dict()
+
+    dict_model = model.to_dict()
+    assert dict_model['a'] == 1
+    assert dict_model['d'][0]['e'] == 6
+    assert dict_model['createdAt'] == 'blabla'
