cfaa697123ab76d0f925ddd113cfa1b0fa8b691c
– sanz <sanzbernward@gmail.com>
2024-02-01 19:13:15 +0800

    Improve URL detection in auto_hyperlinks example (#250)
    Co-authored-by: Ilya Siamionau <ilya@marshal.dev>

--- examples/advanced_usage/auto_hyperlinks.py
+++ examples/advanced_usage/auto_hyperlinks.py
@@ -4,19 +4,16 @@ import typing as t
 from atproto import Client, models
 
 
-def extract_url_byte_positions(
-    text: str, *, aggressive: bool, encoding: str = 'UTF-8'
-) -> t.List[t.Tuple[str, int, int]]:
-    """If aggressive is False, only links beginning http or https will be detected"""
+def extract_url_byte_positions(text: str, *, encoding: str = 'UTF-8') -> t.List[t.Tuple[str, int, int]]:
+    """This function will detect any links beginning with http or https."""
     encoded_text = text.encode(encoding)
 
-    if aggressive:
-        pattern = rb'(?:[\w+]+\:\/\/)?(?:[\w\d-]+\.)*[\w-]+[\.\:]\w+\/?(?:[\/\?\=\&\#\.]?[\w-]+)+\/?'
-    else:
-        pattern = rb'https?\:\/\/(?:[\w\d-]+\.)*[\w-]+[\.\:]\w+\/?(?:[\/\?\=\&\#\.]?[\w-]+)+\/?'
+    # Adjusted URL matching pattern
+    pattern = rb'https?://[^ \n\r\t]*'
 
     matches = re.finditer(pattern, encoded_text)
     url_byte_positions = []
+
     for match in matches:
         url_bytes = match.group(0)
         url = url_bytes.decode(encoding)
@@ -29,29 +26,23 @@ def main() -> None:
     client = Client()
     client.login('my-handle', 'my-password')
 
-    text = 'Example post with automatic link detection https://github.com/MarshalX/atproto, google.com and apple.com'
+    # AT requires URL to include http or https when creating the facet
+    text = 'Example post with automatic link detection https://github.com/MarshalX/atproto and http://atproto.blue'
 
     # Determine locations of URLs in the post's text
-    url_positions = extract_url_byte_positions(text, aggressive=True)
+    url_positions = extract_url_byte_positions(text)
     facets = []
 
-    # AT requires URL to include http or https when creating the facet. Appends to URL if not present
-    for link in url_positions:
-        uri = link[0] if link[0].startswith('http') else f'https://{link[0]}'
+    for link_data in url_positions:
+        uri, byte_start, byte_end = link_data
         facets.append(
             models.AppBskyRichtextFacet.Main(
                 features=[models.AppBskyRichtextFacet.Link(uri=uri)],
-                index=models.AppBskyRichtextFacet.ByteSlice(byte_start=link[1], byte_end=link[2]),
+                index=models.AppBskyRichtextFacet.ByteSlice(byte_start=byte_start, byte_end=byte_end),
             )
         )
 
-    client.com.atproto.repo.create_record(
-        models.ComAtprotoRepoCreateRecord.Data(
-            repo=client.me.did,
-            collection=models.ids.AppBskyFeedPost,
-            record=models.AppBskyFeedPost.Main(created_at=client.get_current_time_iso(), text=text, facets=facets),
-        )
-    )
+    client.send_post(text, facets=facets)
 
 
 if __name__ == '__main__':
