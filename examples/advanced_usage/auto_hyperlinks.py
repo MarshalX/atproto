c3b6ff15dfce4c07da19639acb4acf5c98679e2d
Ilya (Marshal) <ilya@marshal.dev>
2024-02-01 12:09:03 +0100

    cleanup example

--- examples/advanced_usage/auto_hyperlinks.py
+++ examples/advanced_usage/auto_hyperlinks.py
@@ -4,10 +4,8 @@ import typing as t
 from atproto import Client, models
 
 
-def extract_url_byte_positions(
-    text: str, *, encoding: str = 'UTF-8'
-) -> t.List[t.Tuple[str, int, int]]:
-    """This function will detect any links beginning with http or https"""
+def extract_url_byte_positions(text: str, *, encoding: str = 'UTF-8') -> t.List[t.Tuple[str, int, int]]:
+    """This function will detect any links beginning with http or https."""
     encoded_text = text.encode(encoding)
 
     # Adjusted URL matching pattern
@@ -23,33 +21,29 @@ def extract_url_byte_positions(
 
     return url_byte_positions
 
+
 def main() -> None:
     client = Client()
     client.login('my-handle', 'my-password')
 
-    text = 'Example post with automatic link detection https://github.com/MarshalX/atproto, http://google.com and https://apple.com'
+    # AT requires URL to include http or https when creating the facet
+    text = 'Example post with automatic link detection https://github.com/MarshalX/atproto and http://atproto.blue'
 
     # Determine locations of URLs in the post's text
     url_positions = extract_url_byte_positions(text)
     facets = []
 
-    # AT requires URL to include http or https when creating the facet
-    for link in url_positions:
-        uri = link[0]
+    for link_data in url_positions:
+        uri, byte_start, byte_end = link_data
         facets.append(
             models.AppBskyRichtextFacet.Main(
                 features=[models.AppBskyRichtextFacet.Link(uri=uri)],
-                index=models.AppBskyRichtextFacet.ByteSlice(byte_start=link[1], byte_end=link[2]),
+                index=models.AppBskyRichtextFacet.ByteSlice(byte_start=byte_start, byte_end=byte_end),
             )
         )
 
-    client.com.atproto.repo.create_record(
-        models.ComAtprotoRepoCreateRecord.Data(
-            repo=client.me.did,
-            collection=models.ids.AppBskyFeedPost,
-            record=models.AppBskyFeedPost.Main(created_at=client.get_current_time_iso(), text=text, facets=facets),
-        )
-    )
+    client.send_post(text, facets=facets)
+
 
 if __name__ == '__main__':
     main()
