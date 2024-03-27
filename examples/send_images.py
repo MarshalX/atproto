3e7df180cd69954ceff70af30dafb23c70613d2e
ry_Tom <ryotoyota@outlook.jp>
2024-03-28 00:51:43 +0900

    Add example of send_images

---
+++ examples/send_images.py
@@ -0,0 +1,22 @@
+from atproto import Client
+
+
+def main() -> None:
+    client = Client()
+    client.login('my-handle', 'my-password')
+
+    # replace the path to your image file
+    paths = ['cat.jpg', 'dog.jpg', 'bird.jpg']
+    images = []
+    for path in paths:
+        with open(path, 'rb') as f:
+            images.append(f.read())
+
+    client.send_images(
+        text='Post with image from Python',
+        images=images,
+        image_alts=['Text version', 'of the image (ALT)', 'This parameter is optional'])
+
+
+if __name__ == '__main__':
+    main()
