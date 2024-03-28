68c1a73423320385964e7178a01a9e7b07bc45cd
ry_Tom <ryotoyota@outlook.jp>
2024-03-29 00:17:02 +0900

    make a variable for list of image alt

--- examples/send_images.py
+++ examples/send_images.py
@@ -7,6 +7,8 @@ def main() -> None:
 
     # replace the path to your image file
     paths = ['cat.jpg', 'dog.jpg', 'bird.jpg']
+    image_alts = ['Text version', 'of the image (ALT)', 'This parameter is optional']
+
     images = []
     for path in paths:
         with open(path, 'rb') as f:
@@ -15,7 +17,7 @@ def main() -> None:
     client.send_images(
         text='Post with image from Python',
         images=images,
-        image_alts=['Text version', 'of the image (ALT)', 'This parameter is optional'])
+        image_alts=image_alts)
 
 
 if __name__ == '__main__':
