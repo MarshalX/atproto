21859465f7d0761130f7c75bef61639fea7ea377
ry_Tom <ryotoyota@outlook.jp>
2024-03-30 15:47:08 +0900

    auto-generate async_client

--- packages/atproto_client/client/async_client.py
+++ packages/atproto_client/client/async_client.py
@@ -4,6 +4,7 @@
 # This file is part of Python atproto SDK. Licenced under MIT.
 ##################################################################
 
+import asyncio
 import typing as t
 from asyncio import Lock
 
@@ -181,6 +182,58 @@ class AsyncClient(
         uri = AtUri.from_str(post_uri)
         return await self.app.bsky.feed.post.delete(uri.hostname, uri.rkey)
 
+    async def send_images(
+        self,
+        text: t.Union[str, TextBuilder],
+        images: t.List[bytes],
+        image_alts: t.Optional[t.List[str]] = None,
+        profile_identify: t.Optional[str] = None,
+        reply_to: t.Optional['models.AppBskyFeedPost.ReplyRef'] = None,
+        langs: t.Optional[t.List[str]] = None,
+        facets: t.Optional[t.List['models.AppBskyRichtextFacet.Main']] = None,
+    ) -> 'models.AppBskyFeedPost.CreateRecordResponse':
+        """Send post with multiple attached images (up to 4 images).
+
+        Note:
+            If `profile_identify` is not provided will be sent to the current profile.
+
+        Args:
+            text: Text of the post.
+            images: List of binary images to attach. The length must be less than or equal to 4.
+            image_alts: List of text version of the images.
+                        The length should be shorter than or equal to the length of `images`.
+            profile_identify: Handle or DID. Where to send post.
+            reply_to: Root and parent of the post to reply to.
+            langs: List of used languages in the post.
+            facets: List of facets (rich text items).
+
+        Returns:
+            :obj:`models.AppBskyFeedPost.CreateRecordResponse`: Reference to the created record.
+
+        Raises:
+            :class:`atproto.exceptions.AtProtocolError`: Base exception.
+        """
+        if image_alts is None:
+            image_alts = [''] * len(images)
+        else:
+            # padding with empty string if len is insufficient
+            diff = len(images) - len(image_alts)
+            image_alts = image_alts + [''] * diff  # [''] * (minus) => []
+
+        uploads = await asyncio.gather(*[self.upload_blob(image) for image in images])
+        embed_images = [
+            models.AppBskyEmbedImages.Image(alt=alt, image=upload.blob) for alt, upload in zip(image_alts, uploads)
+        ]
+
+        return await self.send_post(
+            text,
+            profile_identify=profile_identify,
+            reply_to=reply_to,
+            embed=models.AppBskyEmbedImages.Main(images=embed_images),
+            langs=langs,
+            facets=facets,
+        )
+
     async def send_image(
         self,
         text: t.Union[str, TextBuilder],
@@ -199,7 +252,7 @@ class AsyncClient(
         Args:
             text: Text of the post.
             image: Binary image to attach.
-            image_alt: Text version of the image
+            image_alt: Text version of the image.
             profile_identify: Handle or DID. Where to send post.
             reply_to: Root and parent of the post to reply to.
             langs: List of used languages in the post.
@@ -211,13 +264,12 @@ class AsyncClient(
         Raises:
             :class:`atproto.exceptions.AtProtocolError`: Base exception.
         """
-        upload = await self.com.atproto.repo.upload_blob(image)
-        images = [models.AppBskyEmbedImages.Image(alt=image_alt, image=upload.blob)]
-        return await self.send_post(
+        return await self.send_images(
             text,
+            images=[image],
+            image_alts=[image_alt],
             profile_identify=profile_identify,
             reply_to=reply_to,
-            embed=models.AppBskyEmbedImages.Main(images=images),
             langs=langs,
             facets=facets,
         )
