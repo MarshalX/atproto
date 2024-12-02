517353e9bf3f94807d7ed4f0d8857d0dc5d2cd30
clarencepalmer <cole@rollprogramcole.com>
2024-12-02 08:19:09 -0800

    revert: single quotes

--- examples/send_image.py
+++ examples/send_image.py
@@ -3,21 +3,21 @@ from atproto import Client, models
 
 def main() -> None:
     client = Client()
-    client.login("my-handle", "my-password")
+    client.login('my-handle', 'my-password')
 
     # replace the path to your image file
-    with open("cat.jpg", "rb") as f:
+    with open('cat.jpg', 'rb') as f:
         img_data = f.read()
 
     aspect_ratio = models.AppBskyEmbedDefs.AspectRatio(height=100, width=100)
 
     client.send_image(
-        text="Post with image from Python",
+        text='Post with image from Python',
         image=img_data,
-        image_alt="Text version of the image (ALT)",
+        image_alt='Text version of the image (ALT)',
         image_aspect_ratio=aspect_ratio,
     )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
