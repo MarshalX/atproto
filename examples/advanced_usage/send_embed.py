bc273f2cf747345eeeae62515df17c2b02417f64
zzstoatzz <thrast36@gmail.com>
2024-12-06 00:22:38 -0600

    merge

--- examples/advanced_usage/send_embed.py
+++ examples/advanced_usage/send_embed.py
@@ -1,12 +1,16 @@
+import httpx
 from atproto import Client, models
 
+# condescending cat stare
+DEFAULT_IMAGE_URL = 'https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:xbtmt2zjwlrfegqvch7fboei/bafkreiae4rcokecag5qlpvg6l3otzulqn3hllnazmz2ezyqfl6xzpy5noe@jpeg'
 
-def main() -> None:
+
+def main(handle: str, password: str) -> None:
     client = Client()
-    client.login('my-handle', 'my-password')
+    client.login(handle, password)
 
+    # Example 1: Link card embed
     text = 'Example post with embed external resource (link card)'
-    # AppBskyEmbedExternal is the same as "link card" in the app
     embed_external = models.AppBskyEmbedExternal.Main(
         external=models.AppBskyEmbedExternal.External(
             title='Google',
@@ -14,15 +18,32 @@ def main() -> None:
             uri='https://google.com',
         )
     )
-
     post_with_link_card = client.send_post(text=text, embed=embed_external)
 
+    # Example 2: Simple quote post
     text_quote = 'Example post with embed post and quote (quote post)'
-    # AppBskyEmbedRecord is the same as "quote post" in the app
     embed_post = models.AppBskyEmbedRecord.Main(record=models.create_strong_ref(post_with_link_card))
-
     client.send_post(text=text_quote, embed=embed_post)
+
+    # Example 3: Quote post with image
+    img_data = httpx.get(DEFAULT_IMAGE_URL).content
+    uploaded_blob = client.upload_blob(img_data).blob
+
+    embed_post_with_image = models.AppBskyEmbedRecordWithMedia.Main(
+        record=models.AppBskyEmbedRecord.Main(record=models.create_strong_ref(post_with_link_card)),
+        media=models.AppBskyEmbedImages.Main(
+            images=[
+                models.AppBskyEmbedImages.Image(
+                    image=uploaded_blob,
+                    alt='Example image with quote',
+                    aspect_ratio=models.AppBskyEmbedDefs.AspectRatio(width=1, height=1),
+                )
+            ]
+        ),
+    )
+
+    client.send_post(text='Example post with both quote and image', embed=embed_post_with_image)
 
 
 if __name__ == '__main__':
-    main()
+    main(handle='my-handle', password='my-password')  # noqa: S106
