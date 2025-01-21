a58c1a07c13d2eaa511edb46516839e187980468
unknown <yamamotura>
2025-01-21 07:30:03 +0100

    Added video_aspect_ratio to the example

--- examples/send_video.py
+++ examples/send_video.py
@@ -1,4 +1,4 @@
-from atproto import Client
+from atproto import Client, models
 
 
 def main() -> None:
@@ -8,8 +8,17 @@ def main() -> None:
     # replace the path to your video file
     with open('video.mp4', 'rb') as f:
         vid_data = f.read()
+        
+    # Add image aspect ratio to prevent default 1:1 aspect ratio
+    # Replace with your desired aspect ratio
+    aspect_ratio = models.AppBskyEmbedDefs.AspectRatio(height=100, width=100)
 
-    client.send_video(text='Post with video from Python', video=vid_data, video_alt='Text version of the video (ALT)')
+    client.send_video(
+        text='Post with video from Python',
+        video=vid_data,
+        video_alt='Text version of the video (ALT)',
+        video_aspect_ratio=aspect_ratio,
+    )
 
 
 if __name__ == '__main__':
