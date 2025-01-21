727a634d0cde46bd451a143482bda8dfc284368f
unknown <yamamotura>
2025-01-21 07:26:55 +0100

    Added video aspect ratio to send_video

--- packages/atproto_client/client/client.py
+++ packages/atproto_client/client/client.py
@@ -289,6 +289,7 @@ class Client(SessionDispatchMixin, SessionMethodsMixin, TimeMethodsMixin, Header
         reply_to: t.Optional['models.AppBskyFeedPost.ReplyRef'] = None,
         langs: t.Optional[t.List[str]] = None,
         facets: t.Optional[t.List['models.AppBskyRichtextFacet.Main']] = None,
+        video_aspect_ratio: t.Optional['models.AppBskyEmbedDefs.AspectRatio'] = None,
     ) -> 'models.AppBskyFeedPost.CreateRecordResponse':
         """Send post with attached video.
 
@@ -319,7 +320,7 @@ class Client(SessionDispatchMixin, SessionMethodsMixin, TimeMethodsMixin, Header
             text,
             profile_identify=profile_identify,
             reply_to=reply_to,
-            embed=models.AppBskyEmbedVideo.Main(video=upload.blob, alt=video_alt),
+            embed=models.AppBskyEmbedVideo.Main(video=upload.blob, alt=video_alt,aspect_ratio=video_aspect_ratio),
             langs=langs,
             facets=facets,
         )
