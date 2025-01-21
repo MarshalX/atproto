ebacf8b31cf239c966b00e50bcfc1b0479985435
Yoshitura Yamamoto <112742954+yamamotura@users.noreply.github.com>
2025-01-21 11:17:16 +0100

    Update send_video.py

--- examples/send_video.py
+++ examples/send_video.py
@@ -8,7 +8,7 @@ def main() -> None:
     # replace the path to your video file
     with open('video.mp4', 'rb') as f:
         vid_data = f.read()
-        
+
     # Add video aspect ratio to prevent default 1:1 aspect ratio
     # Replace with your desired aspect ratio
     aspect_ratio = models.AppBskyEmbedDefs.AspectRatio(height=100, width=100)
