20d45c5b98665cdd85a94786adb7f2dcb379869e
Ilya (Marshal) <ilya@marshal.dev>
2025-03-23 17:48:16 +0100

    fix NSID validation

--- packages/atproto_core/nsid/nsid.py
+++ packages/atproto_core/nsid/nsid.py
@@ -7,16 +7,16 @@ from atproto_core.exceptions import InvalidNsidError
 _NSID_DELIM = '.'
 _NSID_NAMESPACE = '*'
 
-_MAX_NSID_LEN = 253 + 1 + 128
-_MAX_NSID_DOMAIN_PART = 63
-_MAX_NSID_NAME_PART = 128
-_MIN_NSID_PART = 1
+_MAX_NSID_LEN = 253 + 1 + 63
+_MAX_NSID_PART_LEN = 63
+_MIN_NSID_PART_LEN = 1
 _MIN_NSID_SEGMENTS_COUNT = 3
 
 _NSID_VALID_CHARS_REGEX = r'^[a-zA-Z0-9.-]*$'
-_NSID_NONFINAL_SEGMENT_REGEX = r'^[a-zA-Z][a-zA-Z-]*$'
-_NSID_FINAL_SEGMENT_REGEX = r'^[a-zA-Z][a-zA-Z0-9-]*$'
+_NSID_FIRST_SEGMENT_NO_LEADING_DIGIT_REGEX = r'^[0-9]'
+_NSID_LAST_SEGMENT_REGEX = r'^[a-zA-Z][a-zA-Z0-9]*$'
 
+_NSID_PART_FORBIDDEN_START = '-'
 _NSID_PART_FORBIDDEN_END = '-'
 
 Segments = t.List[str]
@@ -87,37 +87,36 @@ def _exclude_nsid_namespace_if_exists(segments: Segments) -> str:
     return _NSID_DELIM.join(segments)
 
 
-def _validate_nsid_segment(segment: str, is_last_segment: bool) -> bool:
+def _validate_nsid_segment(segment: str, is_first_segment: bool, is_last_segment: bool) -> bool:
     """Validate NSID segment.
 
     Args:
         segment: Segment to validate.
+        is_first_segment: The first segment indicator.
         is_last_segment: The last segment indicator.
 
     Returns:
-        :obj:`bool`: Validation result is always True.
+        :obj:`bool`: The validation result is always True.
 
     Raises:
         :class:`atproto.exceptions.InvalidNsidError`: Invalid NSID exception.
     """
     segment_len = len(segment)
+    if segment_len < _MIN_NSID_PART_LEN:
+        raise InvalidNsidError(f'NSID segment is too short. Min len: {_MIN_NSID_PART_LEN}')
+    if segment_len > _MAX_NSID_PART_LEN:
+        raise InvalidNsidError(f'NSID segment is too long. Max len: {_MAX_NSID_PART_LEN}')
 
-    if segment_len < _MIN_NSID_PART:
-        raise InvalidNsidError(f'NSID segment is too short. Min len: {_MIN_NSID_PART}')
-
-    if is_last_segment:
-        if segment_len > _MAX_NSID_NAME_PART:
-            raise InvalidNsidError(f'NSID name segment is too long. Max len: {_MAX_NSID_NAME_PART}')
-        if not re.search(_NSID_FINAL_SEGMENT_REGEX, segment):
-            raise InvalidNsidError('NSID name segments must start with a letter and may contain non-leading digits.')
-    else:
-        if segment_len > _MAX_NSID_DOMAIN_PART:
-            raise InvalidNsidError(f'NSID domain segment is too long. Max len: {_MAX_NSID_DOMAIN_PART}')
-        if not re.search(_NSID_NONFINAL_SEGMENT_REGEX, segment):
-            raise InvalidNsidError('NSID non-final segments must start with ASCII letter and cannot contain digits')
-
+    if segment.startswith(_NSID_PART_FORBIDDEN_START):
+        raise InvalidNsidError(f'NSID segment cannot start with {_NSID_PART_FORBIDDEN_START}')
     if segment.endswith(_NSID_PART_FORBIDDEN_END):
-        raise InvalidNsidError(f'NSID segment contains invalid end symbol: {_NSID_PART_FORBIDDEN_END}')
+        raise InvalidNsidError(f'NSID segment cannot end with {_NSID_PART_FORBIDDEN_END}')
+
+    if is_first_segment and re.search(_NSID_FIRST_SEGMENT_NO_LEADING_DIGIT_REGEX, segment):
+        raise InvalidNsidError('NSID first segment cannot start with a digit')
+
+    if is_last_segment and not re.search(_NSID_LAST_SEGMENT_REGEX, segment):
+        raise InvalidNsidError('NSID final segments must start with a letter and contain only letters and digits')
 
     return True
 
@@ -134,23 +133,20 @@ def _validate_nsid(nsid: str) -> bool:
     Raises:
         :class:`atproto.exceptions.InvalidNsidError`: Invalid NSID exception.
     """
-    segments = get_nsid_segments(nsid)
-    clean_nsid = _exclude_nsid_namespace_if_exists(segments)
-    clean_segments = get_nsid_segments(clean_nsid)
-
-    if not re.search(_NSID_VALID_CHARS_REGEX, clean_nsid):
-        raise InvalidNsidError('NSID contains invalid chars')
-
-    if len(clean_nsid) > _MAX_NSID_LEN:
+    if len(nsid) > _MAX_NSID_LEN:
         raise InvalidNsidError(f'NSID is too long. Max len: {_MAX_NSID_LEN}')
+    if not re.search(_NSID_VALID_CHARS_REGEX, nsid):
+        raise InvalidNsidError('NSID contains invalid chars. Only letters, digits, dots, and hyphens are allowed')
 
+    segments = get_nsid_segments(nsid)
     if len(segments) < _MIN_NSID_SEGMENTS_COUNT:
-        raise InvalidNsidError(f'NSID must contain at least {_MIN_NSID_SEGMENTS_COUNT} segments')
+        raise InvalidNsidError(f'NSID must contains at least {_MIN_NSID_SEGMENTS_COUNT} segments')
 
-    segments_count = len(clean_segments)
-    for segment_index, segment in enumerate(clean_segments):
+    segments_count = len(segments)
+    for segment_index, segment in enumerate(segments):
+        is_first_segment = segment_index == 0
         is_last_segment = segment_index + 1 == segments_count
-        _validate_nsid_segment(segment, is_last_segment)
+        _validate_nsid_segment(segment, is_first_segment, is_last_segment)
 
     return True
 
