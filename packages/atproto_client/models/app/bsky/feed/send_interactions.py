602e8e2d7ef55b10c11a4656d6e35f657142ff8f
Ilya (Marshal) <ilya@marshal.dev>
2026-03-04 15:55:58 +0000

    Update lexicons fetched from 137065b committed 2026-03-02T19:27:09Z

--- packages/atproto_client/models/app/bsky/feed/send_interactions.py
+++ packages/atproto_client/models/app/bsky/feed/send_interactions.py
@@ -7,6 +7,10 @@
 
 import typing as t
 
+import typing_extensions as te
+
+from atproto_client.models import string_formats
+
 if t.TYPE_CHECKING:
     from atproto_client import models
 from atproto_client.models import base
@@ -16,10 +20,12 @@ class Data(base.DataModelBase):
     """Input data model for :obj:`app.bsky.feed.sendInteractions`."""
 
     interactions: t.List['models.AppBskyFeedDefs.Interaction']  #: Interactions.
+    feed: t.Optional[string_formats.AtUri] = None  #: Feed.
 
 
 class DataDict(t.TypedDict):
     interactions: t.List['models.AppBskyFeedDefs.Interaction']  #: Interactions.
+    feed: te.NotRequired[t.Optional[string_formats.AtUri]]  #: Feed.
 
 
 class Response(base.ResponseModelBase):
